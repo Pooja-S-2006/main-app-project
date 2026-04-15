9652037098218c87b3282bc74967573e5a20a9d8
Pooja-S-2006 <poojasenthil@1521>
2026-04-15 20:55:58 +0530

    Integrated submodule functions into main app

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
-print("Main Application Started")
+import sys
+import os
+
+# Add submodule path
+sys.path.append(os.path.join(os.path.dirname(__file__), 'libs/math-lib-project'))
+
+from calc import add, sub, mul, div
+
+print("Addition:", add(10, 5))
+print("Subtraction:", sub(10, 5))
+print("Multiplication:", mul(10, 5))
+print("Division:", div(10, 5))
